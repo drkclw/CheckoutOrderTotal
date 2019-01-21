c87e9ebd4d62c647c225d7f54df0017940baa241
Sam Gomez <smgomam@gmail.com>
2019-01-20 21:17:43 -0600

    Created MarkdownController and MarkdownController tests. Added test to verify that calling GetAllMarldowns returns list of markdowns.

---
+++ ProductService/Controllers/MarkdownController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductService.Models;
+
+namespace ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarkdownController : ControllerBase
+    {
+        private IRepository<Markdown> markdownRepository;
+
+        public MarkdownController(IRepository<Markdown> repository)
+        {
+            markdownRepository = repository;
+        }
+
+        // GET api/allprices
+        [HttpGet]
+        [Route("allmarkdowns")]
+        public ActionResult<IEnumerable<Markdown>> GetAllMarkdowns()
+        {
+            return markdownRepository.GetAll().ToList();
+        }
+    }
+}
---
+++ ProductServiceTests/MarkdownControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using ProductService.Controllers;
+using ProductService.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductServiceTests
+{
+    public class MarkdownControllerTests
+    {
+        private Markdown validMarkdown;
+        private List<Markdown> markdownList;
+
+        [SetUp]
+        public void Setup()
+        {
+            validMarkdown = new Markdown
+            {
+                ProductName = "Can of soup",
+                Amount = 0.45f
+            };
+
+            markdownList = new List<Markdown>();
+            markdownList.Add(validMarkdown);
+        }
+
+        [Test]
+        public void GetAllMarkdownsReturnsListOfMarkdowns()
+        {
+            Mock<IRepository<Markdown>> mockMarkdownRepository = new Mock<IRepository<Markdown>>();
+            mockMarkdownRepository.Setup(x => x.GetAll()).Returns(markdownList);
+
+            MarkdownController markdownController = new MarkdownController(mockMarkdownRepository.Object);
+            var result = markdownController.GetAllMarkdowns();
+            var contentResult = result as ActionResult<IEnumerable<Markdown>>;
+
+            Assert.NotNull(contentResult.Value);
+        }
+    }
+}
