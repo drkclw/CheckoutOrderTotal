8685644f0b0cc0e27683a85ac766d1b8eadec65c
smgomam@gmail.com <smgomam@gmail.com>
2019-02-11 11:45:03 -0600

    Added test to verify that calling GetAll in the price data accessor returns a list of prices.

---
+++ ProductService/Models/Prices/PriceDataAccessor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductService.Models.Prices
+{
+    public class PriceDataAccessor : IDataAccessor<Product>
+    {
+        private IRepository<Product> _productRepository;
+
+        public PriceDataAccessor(IRepository<Product> productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IList<Product> GetAll()
+        {
+            return new List<Product>();
+        }
+        public Product GetByProductName(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Save(Product saveThis)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product deleteThis)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Product updateThis)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProductServiceTests/PriceDataAccessorTests.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using NUnit.Framework;
+using ProductService.Models;
+using ProductService.Models.Prices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductServiceTests
+{
+    public class PriceDataAccessorTests
+    {
+        private Product validProduct;
+        private List<Product> productList;
+
+        [SetUp]
+        public void Setup()
+        {
+            validProduct = new Product
+            {
+                ProductName = "Can of soup",
+                Price = 2.50f,
+                Unit = Unit.EA
+            };
+
+            productList = new List<Product>();
+            productList.Add(validProduct);
+        }
+
+        [Test]
+        public void GetAllPricesReturnsListOfMarkdowns()
+        {            
+            Mock<IRepository<Product>> mockPriceRepository = new Mock<IRepository<Product>>();
+            mockPriceRepository.Setup(x => x.GetAll()).Returns(productList);
+
+            PriceDataAccessor priceDataAccessor = new PriceDataAccessor(mockPriceRepository.Object);
+            var prices = priceDataAccessor.GetAll();
+
+            Assert.NotNull(prices);
+        }
+    }
+}
