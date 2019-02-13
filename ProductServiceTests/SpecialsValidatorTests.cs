f0d8ca9bb9f4dc3e45fac64985442f906664bae8
smgomam@gmail.com <smgomam@gmail.com>
2019-02-13 15:07:31 -0600

    Added test to verify that validating a valid limit special returns success.

--- ProductServiceTests/SpecialsValidatorTests.cs
+++ ProductServiceTests/SpecialsValidatorTests.cs
@@ -12,6 +12,7 @@ namespace ProductServiceTests
         private PriceSpecial validPriceSpecial;
         private PriceSpecial zeroPriceSpecial;
         private PriceSpecial lessThanTwoQuantityPriceSpecial;
+        private LimitSpecial validLimitSpecial;
 
         [SetUp]
         public void Setup()
@@ -19,6 +20,7 @@ namespace ProductServiceTests
             validPriceSpecial = new PriceSpecial("Can of soup", 2, true, 5);
             zeroPriceSpecial = new PriceSpecial("Can of soup", 2, true, 0);
             lessThanTwoQuantityPriceSpecial = new PriceSpecial("Can of soup", 1, true, 5);
+            validLimitSpecial = new LimitSpecial("Can of beans", 2, true, 1, 0.5f, 6);
         }
 
         [Test]
@@ -52,6 +54,17 @@ namespace ProductServiceTests
 
             Assert.AreEqual(result.IsValid, false);
             Assert.AreEqual(result.Message, "Error: Purchase quantity must be bigger than 1.");
+        }
+
+        [Test]
+        public void ValidateValidLimitSpecialReturnsSuccess()
+        {
+            IValidator<ISpecial> specialsValidator = new SpecialsValidator();
+
+            var result = specialsValidator.Validate(validLimitSpecial);
+
+            Assert.AreEqual(result.IsValid, true);
+            Assert.AreEqual(result.Message, "Success.");
         }
     }
 }
